6954fbd8f52fda803ce8a2899c3262be89ad946b
j4profit <jaynappo@icloud.com>
2025-10-06 16:27:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ async function hangup(callSid, options = {}) {
 
             const hangupUrl = BASE_URL ?
                 BASE_URL + '/hangup-twiml?call_sid=' + callSid :
-                'https://ring2tech.com.com/hangup-twiml?call_sid=' + callSid;
+                'https://ring2tech.com/hangup-twiml?call_sid=' + callSid;
 
             await twilioClient.calls(callSid).update({
                 url: hangupUrl,
